534a1e3760f5a9277a8eb3c14625195099fca854
Libe82 <li.jg82@gmail.com>
2025-02-10 13:27:25 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import CustomSearch from '../CustomSearch';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Your Custom Search Tool</h1>
+      <p>Use the search bar to find relevant news.</p>
+      <CustomSearch />
+    </div>
+  );
+}
